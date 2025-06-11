64c4d8efb0f56c7ac6a1c1b22192c40393b241a7
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2025-06-11 06:07:16 +0000

    Second commit
    Java-2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        String filePath = "file_example_WAV_5MG.wav";
+        String filePath = "src\\file_example_WAV_5MG.wav";
         File file = new File(filePath);
 
-        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+        try(Scanner scanner = new Scanner(System.in);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
 
+            clip.start();
+            String response = "";
+
+            while(!response.equals("Q")){
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+
+                response = scanner.next().toUpperCase();
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
         }
         catch(UnsupportedAudioFileException e){
             System.out.println("Audio file not supported");
         }
+        catch (LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
         catch(IOException e){
             System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Bye");
         }
     }
 }
